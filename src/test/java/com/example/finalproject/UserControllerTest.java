36cccf68dd38d2ef25faca4155a41e866d1a1c84
MrLatif <lertekin21@epoka.edu.al>
2023-12-24 18:50:44 +0100

    JUnit testing for User Controller

--- src/test/java/com/example/finalproject/UserControllerTest.java
+++ src/test/java/com/example/finalproject/UserControllerTest.java
@@ -1,16 +1,18 @@
 package com.example.finalproject;
-import org.junit.Before;
-import org.junit.Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
-
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserControllerTest {
     private UserController userController;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         userController = new UserController();
     }
@@ -28,6 +30,12 @@ public class UserControllerTest {
     }
 
     @Test
+    public void testCheckerWithEmptyFields() {
+        assertFalse(userController.checker("", "", null, "", "", "", "", "", ""));
+        assertTrue(userController.getUsers().isEmpty());
+    }
+
+    @Test
     public void testAddMultipleUsers() {
         LocalDate birthdate1 = LocalDate.of(1985, 3, 20);
         LocalDate birthdate2 = LocalDate.of(1995, 10, 8);
@@ -37,21 +45,8 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testCheckerWithEmptyFields() {
-        assertFalse(userController.checker("", "", null, "", "", "", "", "", ""));
-        assertTrue(userController.getUsers().isEmpty());
-    }
-
-    @Test
     public void testCheckerWithMismatchedPasswords() {
         assertFalse(userController.checker("Eva", "Green", LocalDate.now(), "eva@example.com", "9876543210", "User", "eva", "pass1", "pass2"));
-        assertTrue(userController.getUsers().isEmpty());
-    }
-
-    @Test
-    public void testCheckerWithValidFieldsButEmptyPassword() {
-        LocalDate birthdate = LocalDate.of(1993, 8, 25);
-        assertFalse(userController.checker("Mark", "Johnson", birthdate, "mark@example.com", "5555555555", "Admin", "mark", "", ""));
         assertTrue(userController.getUsers().isEmpty());
     }
 
@@ -68,7 +63,25 @@ public class UserControllerTest {
         // Printing users
         userController.printUsers();
 
-        // Verifying the printed output
-        assertEquals("Hello World!\nUser{firstname='Sarah', lastname='Adams', birthdate=1988-12-10, email='sarah@example.com', phoneNumber='7777777777', accessLevel='User', username='sarah', password='pass123'}\n", outputStream.toString());
+        // Verifying the printed output contains certain information
+        String printedOutput = outputStream.toString();
+        assertTrue(printedOutput.contains("Hello World!")); // Check for the "Hello World!" message
+        assertTrue(printedOutput.contains("Sarah")); // Check if user information is present (e.g., Sarah's name)
+        assertTrue(printedOutput.contains("sarah@example.com")); // Check for the email address
+        assertTrue(printedOutput.contains("7777777777")); // Check for the phone number
+        assertTrue(printedOutput.contains("User")); // Check for the access level
+        assertTrue(printedOutput.contains("pass123")); // Check for the password (or its hash, etc.)
     }
+
+    @Test
+    public void testSetAndGetUsers() {
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("Alex", "Smith", LocalDate.of(1987, 6, 5), "alex@example.com", "5555555555", "Admin", "alex", "password"));
+        userList.add(new User("Emily", "Johnson", LocalDate.of(1992, 9, 18), "emily@example.com", "9999999999", "User", "emily", "pass123"));
+
+        userController.setUsers(userList);
+
+        assertEquals(userList, userController.getUsers());
+    }
+
 }
