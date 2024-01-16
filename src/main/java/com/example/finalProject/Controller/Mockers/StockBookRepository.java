d4d53dac2a7826daef1561f3721a682a5af1500f
MrLatif <lertekin21@epoka.edu.al>
2024-01-16 21:55:51 +0100

    change finalProject headline to kthimi

--- src/main/java/com/example/finalProject/Controller/Mockers/StockBookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finalProject.Controller.Mockers;
-
-import com.example.finalProject.Model.BookModel;
-
-import java.util.ArrayList;
-
-public interface StockBookRepository {
-    ArrayList<BookModel> getStockBooks();
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,9 @@ module com.example.kthimi {
     requires javafx.fxml;
 
 
-    opens com.example.finalProject to javafx.fxml;
-    exports com.example.finalProject;
+    opens com.example.kthimi to javafx.fxml;
+    exports com.example.kthimi;
 
-    opens com.example.finalProject.Model to javafx.base;
+    opens com.example.kthimi.Model to javafx.base;
 }
 
